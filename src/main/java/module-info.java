78b9876714027d570f8f8f48472e013551e05fff
obuyajr <felix37brock@gmail.com>
2023-04-27 16:15:49 +0300

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.roombooking {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.example.roombooking to javafx.fxml;
+    exports com.example.roombooking;
+}
